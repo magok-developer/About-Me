6a347a7fbf217603a7bf9d19b1ce9d0724ffdabd
Jiwon <hajw.study@gmail.com>
2024-03-21 01:38:36 +0900

    Project 컴포넌트 생성

---
+++ src/app/components/Project/Project.tsx
@@ -0,0 +1,66 @@
+import { color } from "@/app/styles/color";
+import styled from "@emotion/styled";
+import Image from "next/image";
+import React from "react";
+
+const Project = () => {
+  return (
+    <Container>
+      <div className='title'>Contact</div>
+      <ImagesWrap>
+        <Image
+          src='/images/contact/gitHub.svg'
+          width={48}
+          height={48}
+          alt='gitHub'
+        />
+        <Image
+          src='/images/contact/linkedIn.svg'
+          width={48}
+          height={48}
+          alt='linkedIn'
+        />
+        <Image
+          src='/images/contact/google.svg'
+          width={48}
+          height={48}
+          alt='google'
+        />
+        <Image
+          src='/images/contact/discord.svg'
+          width={48}
+          height={48}
+          alt='discord'
+        />
+        <Image
+          src='/images/contact/instagram.svg'
+          width={48}
+          height={48}
+          alt='instagram'
+        />
+      </ImagesWrap>
+    </Container>
+  );
+};
+
+export default Project;
+
+const Container = styled.div`
+  height: 800px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 160px;
+
+  .title {
+    font-size: 32px;
+    font-weight: bold;
+
+    color: ${color.black};
+  }
+`;
+
+const ImagesWrap = styled.div`
+  display: flex;
+  gap: 60px;
+`;
