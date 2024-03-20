b582d3ea77b94505b0b8088da5b62792e9fe5997
Jiwon <hajw.study@gmail.com>
2024-03-20 18:55:07 +0900

    메인페이지 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,24 @@ import styled from "@emotion/styled";
 import { color } from "./styles/color";
 
 import Home from "./components/Home/Home";
+import AboutMe from "./components/AboutMe/AboutMe";
+import Skills from "./components/Skills/Skills";
+import Contact from "./components/Contact/Contact";
 
 const Page = () => {
   return (
     <Container>
       <Home />
+      <AboutMe />
+      <Skills />
+      <Contact />
     </Container>
   );
 };
 export default Page;
 
 const Container = styled.div`
-  height: 100vh;
+  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
