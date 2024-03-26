9a14bb6b144a9f5ffa78fce8706a16c157ff0704
Jiwon <hajw.study@gmail.com>
2024-03-26 18:03:22 +0900

    contact 컴포넌트 완료

--- src/app/components/Contact/Contact.tsx
+++ src/app/components/Contact/Contact.tsx
@@ -1,47 +1,90 @@
 import { color } from "@/app/styles/color";
 import styled from "@emotion/styled";
 import Image from "next/image";
-import React from "react";
+import Link from "next/link";
+import React, { useState } from "react";
 
 type Props = {
   element: () => void;
 };
 
 const Contact = ({ element }: Props) => {
+  const [clickedIndex, setClickedIndex] = useState(-1); // 클릭된 이미지의 인덱스를 저장합니다.
+
+  const icon = [
+    {
+      icon: "/images/contact/gitHub.svg",
+      activeIcon: "/images/contact/gitHub_active.svg",
+      url: "https://github.com/magok-developer",
+      copy: "",
+    },
+    {
+      icon: "/images/contact/linkedIn.svg",
+      activeIcon: "/images/contact/linkedIn_active.svg",
+      url: "naver.com",
+      copy: "",
+    },
+    {
+      icon: "/images/contact/google.svg",
+      activeIcon: "/images/contact/google_active.svg",
+      url: "",
+      copy: "hajw.study@gmail",
+    },
+    {
+      icon: "/images/contact/discord.svg",
+      activeIcon: "/images/contact/discord_active.svg",
+      url: "",
+      copy: "hajw.study",
+    },
+  ];
+
+  const handleClickCopy = async (index: number, text: string) => {
+    setClickedIndex(index);
+
+    try {
+      if (text !== "") {
+        await navigator.clipboard.writeText(text);
+        alert("클립보드에 주소가 복사 되었습니다.");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <Container ref={element}>
       <div className='title'>Contact</div>
       <ImagesWrap>
-        <Image
-          src='/images/contact/gitHub.svg'
-          width={48}
-          height={48}
-          alt='gitHub'
-        />
-        <Image
-          src='/images/contact/linkedIn.svg'
-          width={48}
-          height={48}
-          alt='linkedIn'
-        />
-        <Image
-          src='/images/contact/google.svg'
-          width={48}
-          height={48}
-          alt='google'
-        />
-        <Image
-          src='/images/contact/discord.svg'
-          width={48}
-          height={48}
-          alt='discord'
-        />
-        <Image
-          src='/images/contact/instagram.svg'
-          width={48}
-          height={48}
-          alt='instagram'
-        />
+        {icon.map((item, index) =>
+          item.url !== "" ? (
+            <Link href={item.url} target='_blank' key={index}>
+              <Image
+                src={clickedIndex === index ? item.activeIcon : item.icon}
+                width={48}
+                height={48}
+                alt='icon'
+                onClick={() => handleClickCopy(index, item.copy)}
+                onMouseEnter={() => setClickedIndex(index)}
+                onMouseLeave={() => setClickedIndex(-1)}
+                style={{ cursor: "pointer" }}
+                className='icon'
+              />
+            </Link>
+          ) : (
+            <Image
+              key={index}
+              src={clickedIndex === index ? item.activeIcon : item.icon}
+              width={48}
+              height={48}
+              alt='icon'
+              onClick={() => handleClickCopy(index, item.copy)}
+              onMouseEnter={() => setClickedIndex(index)}
+              onMouseLeave={() => setClickedIndex(-1)}
+              style={{ cursor: "pointer" }}
+              className='icon'
+            />
+          )
+        )}
       </ImagesWrap>
     </Container>
   );
